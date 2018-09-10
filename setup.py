1efe44b552cdace97ff67e257e017325f5e7232b
Adam Dullage <spam@dullage.com>
2018-09-10 21:23:18 +0100

    Exception Raising, Sandbox Option & None Handling

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='starlingbank',
-    version="1.1",
+    version="1.2",
     description='A python package that provides limited access to the Starling bank API.',  # noqa
     url='https://github.com/Dullage/starlingbank',
     author='Adam Dullage',
