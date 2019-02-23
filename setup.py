519ad5603882844a8aad07084612f3ed6328125b
Adam Dullage <spam@dullage.com>
2019-02-23 12:13:30 +0000

    Updated Setup

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='starlingbank',
-    version="2.0",
-    description='A python package that provides limited access to the Starling bank API.',  # noqa
+    version="3.0",
+    description='An unofficial python package that provides access to parts of the Starling bank API. Designed to be used for personal use (i.e. using personal access tokens).',  # noqa
     url='https://github.com/Dullage/starlingbank',
     author='Adam Dullage',
     author_email='adam@dullage.com',
