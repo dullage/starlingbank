9495456980d5d6d85c4e999a17dc69481067af09
Adam Dullage <spam@dullage.com>
2019-02-27 21:28:17 +0000

    Bump Version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='starlingbank',
-    version="3.0",
+    version="3.1",
     description='An unofficial python package that provides access to parts of the Starling bank API. Designed to be used for personal use (i.e. using personal access tokens).',  # noqa
     url='https://github.com/Dullage/starlingbank',
     author='Adam Dullage',
